c563e460b184119967589d35e9f3281341a0178e
Annie Wang <anniewang2023@gmail.com>
2024-02-04 09:50:45 -0500

    fixed some small things

--- client/src/components/pages/NotFound.jsx
+++ client/src/components/pages/NotFound.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const NotFound = () => {
   return (
     <div class="NotFound-container">
-        <h1>404 Not Found</h1>
-        <p>Oops! Sorry, it seems like the page you requested does not exist on our server...</p>
-        <div class="NotFound-shapes">
-            <div class="NotFound-circle"></div>
-        </div>
+      <h1>404 Not Found</h1>
+      <p className="NotFound-p">
+        Oops! Sorry, it seems like the page you requested does not exist on our server...
+      </p>
+      <div class="NotFound-shapes">
+        <div class="NotFound-circle"></div>
+      </div>
     </div>
   );
 };
